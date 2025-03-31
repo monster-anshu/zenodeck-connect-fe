ba1bc81dc5374b0c97a03d1c587629fa3d04bd41
Himanshu Gunwant <hgunwant2312@gmail.com>
2025-03-31 07:31:04 +0000

    autosizeable textarea

--- packages/ui/src/components/textarea.tsx
+++ packages/ui/src/components/textarea.tsx
@@ -6,13 +6,34 @@ const Textarea = React.forwardRef<
   HTMLTextAreaElement,
   React.ComponentProps<"textarea">
 >(({ className, ...props }, ref) => {
+  const innerRef = React.useRef<HTMLTextAreaElement>(null);
+
+  const adjustTextareaHeight = () => {
+    const textarea = innerRef.current;
+    if (!textarea) return;
+    const offsetBorder = 2;
+    textarea.style.height = "auto";
+    textarea.style.height = textarea.scrollHeight + offsetBorder + "px";
+  };
+
+  React.useEffect(() => {
+    adjustTextareaHeight();
+  }, []);
+
   return (
     <textarea
       className={cn(
-        "border-input placeholder:text-muted-foreground focus-visible:ring-ring flex min-h-[60px] w-full rounded-md border bg-transparent px-3 py-2 text-base shadow-sm focus-visible:outline-none focus-visible:ring-1 disabled:cursor-not-allowed disabled:opacity-50 md:text-sm",
+        "border-input placeholder:text-muted-foreground focus-visible:ring-ring flex min-h-[60px] w-full resize-none rounded-md border bg-transparent px-3 py-2 text-base shadow-sm focus-visible:outline-none focus-visible:ring-1 disabled:cursor-not-allowed disabled:opacity-50 md:text-sm",
         className
       )}
-      ref={ref}
+      onInput={adjustTextareaHeight}
+      ref={(element) => {
+        innerRef.current = element;
+        if (ref) {
+          (ref as React.RefObject<HTMLTextAreaElement | null>).current =
+            element;
+        }
+      }}
       {...props}
     />
   );
