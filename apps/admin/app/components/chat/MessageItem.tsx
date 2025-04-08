9030282647b294f471db212a62db176196aaa8c0
Himanshu Gunwant <hgunwant2312@gmail.com>
2025-04-08 12:08:13 +0000

    add icon

--- apps/admin/app/components/chat/MessageItem.tsx
+++ apps/admin/app/components/chat/MessageItem.tsx
@@ -1,5 +1,7 @@
 import { Message } from "@repo/chat/schema";
+import { getFormattedTimeDifference } from "@repo/chat/utils/time";
 import { FC } from "react";
+import { LuCheckCheck, LuClock } from "react-icons/lu";
 
 type IMessageItemProps = {
   message: Message;
@@ -7,6 +9,7 @@ type IMessageItemProps = {
 
 const MessageItem: FC<IMessageItemProps> = ({ message }) => {
   const isRight = message.isRight;
+  const diff = getFormattedTimeDifference(new Date(message.timestamp));
 
   return (
     <div className="px-2">
@@ -15,11 +18,22 @@ const MessageItem: FC<IMessageItemProps> = ({ message }) => {
           marginRight: isRight ? "0px" : "auto",
           marginLeft: isRight ? "auto" : "0px",
         }}
-        className="w-fit max-w-[70%] rounded-sm bg-white px-3 py-2 text-sm"
+        className="w-fit max-w-[70%] rounded-sm bg-white px-3 pb-0.5 pt-1.5 text-sm"
       >
-        <p className="whitespace-break-spaces break-all">
-          {message.messageData?.message}
-        </p>
+        <div className="flex items-end">
+          <p className="mb-4 whitespace-break-spaces break-all text-xs">
+            {message.messageData?.message}
+          </p>
+          <p className="text-foreground/60 float-right flex items-center gap-[2px]">
+            <span className="text-[8px]">{diff}</span>
+            {isRight && (
+              <div className="ml-auto w-4">
+                {message.status === "PENDING" && <LuClock size={10} />}
+                {message.status === "DELIVERED" && <LuCheckCheck size={12} />}
+              </div>
+            )}
+          </p>
+        </div>
       </div>
     </div>
   );
