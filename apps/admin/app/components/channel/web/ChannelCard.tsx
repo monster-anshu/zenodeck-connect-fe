c8f69f8baab28b393031f5f2aaf466769b08fecc
Himanshu Gunwant <hgunwant2312@gmail.com>
2025-04-19 11:57:02 +0000

    add channel card

---
+++ apps/admin/app/components/channel/web/ChannelCard.tsx
@@ -0,0 +1,58 @@
+import { Channel } from "@admin-services/channel.service";
+import { Button } from "@repo/ui/components/button";
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuTrigger,
+} from "@repo/ui/components/dropdown-menu";
+import { Switch } from "@repo/ui/components/switch";
+import { FC } from "react";
+import { LuEllipsisVertical } from "react-icons/lu";
+import { Link, useNavigate } from "react-router";
+
+type IChannelCardProps = {
+  channel: Channel;
+};
+
+const ChannelCard: FC<IChannelCardProps> = ({ channel }) => {
+  const navigate = useNavigate();
+  return (
+    <div className="rounded-lg border p-4 text-sm" key={channel._id}>
+      <div className="mb-10 flex items-center gap-2">
+        <p className="flex-1">{channel.name}</p>
+        <Switch />
+      </div>
+      <div className="flex items-center gap-2">
+        <p className="text-foreground/60 flex-1 self-end text-xs">
+          Created by : System
+        </p>
+        <Link
+          to={{ pathname: `/channels/web/${channel._id}`, search: "tab=embed" }}
+        >
+          <Button variant={"outline"}>Embed code</Button>
+        </Link>
+        <DropdownMenu>
+          <DropdownMenuTrigger>
+            <LuEllipsisVertical size={18} />
+          </DropdownMenuTrigger>
+          <DropdownMenuContent>
+            <DropdownMenuItem
+              onClick={() =>
+                navigate({
+                  pathname: `/channels/web/${channel._id}`,
+                  search: "tab=general",
+                })
+              }
+            >
+              Edit
+            </DropdownMenuItem>
+            <DropdownMenuItem>Duplicate</DropdownMenuItem>
+          </DropdownMenuContent>
+        </DropdownMenu>
+      </div>
+    </div>
+  );
+};
+
+export default ChannelCard;
--- apps/admin/app/routes/channels.web.tsx
+++ apps/admin/app/routes/channels.web.tsx
@@ -1,11 +1,8 @@
+import ChannelCard from "@admin-components/channel/web/ChannelCard";
 import { channelQuery } from "@admin-queries/channel.query";
-import { Button } from "@repo/ui/components/button";
 import { Spinner } from "@repo/ui/components/spinner";
-import { Switch } from "@repo/ui/components/switch";
 import { useQuery } from "@tanstack/react-query";
 import { FC } from "react";
-import { LuEllipsisVertical } from "react-icons/lu";
-import { Link } from "react-router";
 
 type IChannelWebPageProps = {};
 
@@ -17,28 +14,7 @@ const ChannelWebPage: FC<IChannelWebPageProps> = () => {
       {isLoading && <Spinner />}
       <div className="container grid md:grid-cols-2 xl:grid-cols-3">
         {data?.channels.map((channel) => {
-          return (
-            <div className="rounded-lg border p-4 text-sm" key={channel._id}>
-              <div className="mb-10 flex items-center gap-2">
-                <p className="flex-1">{channel.name}</p>
-                <Switch />
-              </div>
-              <div className="flex items-end gap-2">
-                <p className="text-foreground/60 flex-1 text-xs">
-                  Created by : System
-                </p>
-                <Link to={`/channels/web/${channel._id}?tab=embed`}>
-                  <Button variant={"outline"}>Embed code</Button>
-                </Link>
-                <Link
-                  className="block rounded border px-0.5 py-2"
-                  to={`/channels/web/${channel._id}?tab=general`}
-                >
-                  <LuEllipsisVertical />
-                </Link>
-              </div>
-            </div>
-          );
+          return <ChannelCard key={channel._id} channel={channel} />;
         })}
       </div>
     </div>
